56439559ba27b6a8257a284a788f4ac0c543bd51
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-27 18:37:07 +0100

    Add signup, logout and private area

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth import views as auth_views
+from main import views
 
 urlpatterns = patterns('',
-    url(r'^$', auth_views.login, {'template_name': 'main/login.html'})
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', auth_views.login, {'template_name': 'main/login.html'}),
+    url(r'^logout/$', auth_views.logout_then_login, {'login_url': '/login/'},
+        name='logout'),
+    url(r'^signup/$', views.signup, name='signup')
 )
--- main/views.py
+++ main/views.py
@@ -1,1 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.models import User
+
+def index(request):
+    if request.user.is_authenticated():
+        return render(request, 'main/logged.html', {'user': request.user})
+    return redirect('login/')
+
+def signup(request):
+    if request.user.is_authenticated():
+        return redirect('/')
+    if request.method == 'POST':
+        # Sign up
+        if request.POST['password'] != request.POST['password_confirm']:
+            return render(request, 'main/signup.html', {'errors': True})
+        User.objects.create_user(
+            request.POST['username'],
+            request.POST['email'],
+            request.POST['password'],
+            first_name=request.POST['first_name'],
+            last_name=request.POST['last_name']
+            )
+        return redirect('/')
+    return render(request, 'main/signup.html')
