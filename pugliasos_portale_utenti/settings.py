289ce6d1d1e6fc60d067f292cdd03e02ffcd524a
Mario Lamacchia <mariolamacchia@gmail.com>
2014-12-12 14:26:07 +0100

    Fix static files dirs

--- pugliasos_portale_utenti/settings.py
+++ pugliasos_portale_utenti/settings.py
@@ -84,6 +84,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    )
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 TEMPLATE_DIRS = (
