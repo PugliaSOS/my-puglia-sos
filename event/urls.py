750b3f169dd5d0a9b1db33c8449fef2333dc6fea
Mario Lamacchia <mariolamacchia@gmail.com>
2014-12-14 17:59:47 +0100

    Create event urls

--- event/urls.py
+++ event/urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls import patterns, include, url
+from event import views
 
 urlpatterns = patterns('',
+    url(r'^$', views.get_all, name='event_list'),
+    url(r'^(?P<event>[0-9]+)/$', views.get_event, name='event'),
+    url(r'^join/(?P<event>[0-9]+)/$', views.join, name='join_event')
 )
--- event/views.py
+++ event/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def get_all(request):
+    pass
+
+def get_event(request, event):
+    pass
+
+def join(request, event):
+    pass
