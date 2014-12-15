86a0fd69eb857188a81c8e53ab1ebb721ac39e71
Mario Lamacchia <mariolamacchia@gmail.com>
2014-12-15 01:56:54 +0100

    Remove last try and fix typo in settings

--- pugliasos_portale_utenti/settings.py
+++ pugliasos_portale_utenti/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     )
--- pugliasos_portale_utenti/urls.py
+++ pugliasos_portale_utenti/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -12,9 +11,3 @@ urlpatterns = patterns('',
     url(r'^events/', include('event.urls')),
     url(r'^polls/', include('poll.urls'))
 )
-
-if not settings.DEBUG:
-    urlpatterns += patterns('', (
-            r'^static/(?P<path>.*)$',
-            'django.views.static.serve', {'document_root': settings.STATIC_ROOT}
-        ),)
