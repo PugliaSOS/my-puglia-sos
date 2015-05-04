a01da1ec4960108cec2dc02347738370c02ff43a
Nicola <nicoladilillo98@gmail.com>
2015-05-04 18:05:56 +0200

    create AssociateStatus (Close #9)

--- social/models.py
+++ social/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class AssociationStatus(models.Model):
+    user = models.ForeignKey(User)
+    year = models.CharField(max_length=4)
+    status = models.BooleanField(default=False)
