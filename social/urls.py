238a593bc778b7d7af496a47bfd00ed196b8fe8a
Messinese Gianluca <gianluca.messi1@gmail.com>
2015-05-09 17:34:44 +0200

    Add Registred_list (views, template,urls)

--- social/urls.py
+++ social/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from event import views
 
 urlpatterns = patterns('',
-    
+    url(r'^$', views.get_registred, name='all_registred'),
 )
--- social/views.py
+++ social/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
+@login_required(login_url='/login/')
+def get_registred(request):
+    all_registred = User.objects.all()
+    return render(request, 'social/registred.html', all_registred)
